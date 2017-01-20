8f3978e1ae8d7a63e0ada020d61f3a8c1e21115d
Stefan Stölzle <stoe@github.com>
2017-01-20 16:25:23 +0100

    Revert "Merge branch 'master' into collaborator-by-issue"
    This reverts commit b7700889d77f3df18b843a1c324473d141f698f7, reversing
    changes made to cd8676ece00ac9b2e6dace6725d52917f7952e5d.
    
    @hectorsector recommended this in https://github.com/githubteacher/practice-sinatra/pull/4#issuecomment-273896779

--- collaborators.rb
+++ collaborators.rb
@@ -7,9 +7,8 @@ class Collaborator
     # Get Issue Commenters and Add as Collaborators
     successfully_added_users = []
     current_collaborators = get_current_collaborators(repo_name)
-    issueComments = client.issue_comments(repo_name, issue_num)
     begin
-      issueComments.each do |comment|
+      client.issue_comments(repo_name, issue_num).each do |comment|
         username = comment[:user][:login]
         puts "adding #{username}"
         next if current_collaborators[username] # skip adding if already a collaborator
@@ -52,12 +51,6 @@ class Collaborator
         abort "ERR posting comment (#{e.inspect})"
       end
     end
-
-    if issueComments.size > 15
-      puts "There are over 15 (#{issueComments.size} to be exact) comments, cleaning up..."
-      removeExtraComments(repo_name, issueComments)
-    end
-
   end
 
   def self.addByIssue(repo_name:, issue_num:, user_login:)
@@ -97,19 +90,4 @@ class Collaborator
       [collaborator[:login], collaborator[:login]]
     }]
   end
-
-  def self.removeExtraComments( repo_name, comments )
-    totalComments = comments.size
-    puts "> Initial size: #{totalComments}"
-    comments.each do |comment|
-      puts "removing comment id #{comment[:id]}"
-      client.delete_comment(repo_name, comment[:id])
-      totalComments -= 1
-      puts "now there are #{totalComments} comments"
-      if totalComments < 15 then
-        break
-      end
-    end
-  end
-
 end
