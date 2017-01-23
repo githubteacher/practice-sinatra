27d4d01c1bfc1dd1132486bcdcc0e984de8887a4
Stefan Stölzle <stoe@github.com>
2017-01-23 15:33:09 +0100

    Remove addByComment from executable basic.rb

--- basics.rb
+++ basics.rb
@@ -13,15 +13,7 @@ post '/payload' do
   issue = push["issue"]["number"].to_i
   user = push["issue"]["user"]["login"].to_s
 
-  if repo === "githubschool/open-enrollment-classes-introduction-to-github" && issue == 927
-    puts "adding to githubschool/open-enrollment-classes-introduction-to-github"
-    Collaborator.addByComment repo_name: "githubschool/open-enrollment-classes-introduction-to-github", issue_num: 927
-  elsif repo ===  "githubschool/on-demand-github-pages" && issue == 1
-    puts "adding to githubschool/on-demand-github-pages"
-    Collaborator.addByComment repo_name: "githubschool/on-demand-github-pages", issue_num: 1
-  else
-    Collaborator.addByIssue repo_name: repo, issue_num: issue, user_login: user
-  end
+  Collaborator.addByIssue repo_name: repo, issue_num: issue, user_login: user
 end
 
 get '/' do
